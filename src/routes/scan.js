479f03a001e4b7e8faea115da012d5fc5c9e64a9
Antenor Pires <antenorpires.6@gmail.com>
2026-01-04 04:30:10 +0000

    fix: adjust scan to use profiles

--- src/routes/scan.js
+++ src/routes/scan.js
@@ -1,31 +1,18 @@
 import express from "express";
-import { runScan } from "../services/scan.js";
+import { runScan } from "../services/scanExecute.js";
 import { sanitizeUrl } from "../utils/sanitize.js";
+import { profiles } from "../services/scanProfiles.js";
 
 const router = express.Router();
 
 router.post("/fast", (req, res) => {
   const target = sanitizeUrl(req.body.target || "");
-  runScan(
-    target, 
-    ``,
-    ``,
-    ``,
-    `nmap -F -Pn -sT --open ${target}`,
-    results => res.json(results)
-  );
+  runScan(target, profiles.fast, res);
 });
 
 router.post("/low", (req, res) => {
   const target = sanitizeUrl(req.body.target || "");
-  runScan(
-    target, 
-    `host -d ${target}`,
-    `whois ${target}`,
-    `dig DNSKEY +dnssec ${target}`,
-    `sudo nmap -F -Pn -sT --open -sV -A -O --script vuln ${target}`,
-    results => res.json(results)
-  );
+  runScan(target, profiles.low, res);
 });
 
 export default router;
